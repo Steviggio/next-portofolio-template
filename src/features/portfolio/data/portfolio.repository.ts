a43b09c7c5cb85414cc7ee9bd9a5b21d3b7efb6a
Steviggio <steve.mothmora@gmail.com>
2026-02-16 19:59:46 +0100

    feat(data): implement repository pattern and type check with Zod

---
+++ src/features/portfolio/data/portfolio.repository.ts
@@ -0,0 +1,135 @@
+import { PortfolioData, PortfolioDataSchema } from "../types/portfolio.schema";
+
+export const RAW_DATA = {
+  profile: {
+    name: "Steve Mothmora",
+    nickname: "Steviggio",
+    title: "Développeur JS | React.js / Node.js",
+    about:
+      "Développeur JavaScript, passionné de langues et de développement, j'aime prendre part à des projets innovants mélant créativité et modernité.",
+    description:
+      "Je suis Steviggio, un développeur Fullstack JS spécialisé dans les technologies modernes telles que Next.js, React, et Node.js. Avec une passion pour la création d'applications web performantes et intuitives, je m'efforce de transformer des idées innovantes en solutions numériques concrètes. En plus de mes compétences techniques, j'ai un intérêt marqué pour les langues et la communication interculturelle, ce qui enrichit ma perspective dans le développement de projets globaux.",
+    location: "Orléans, France",
+    role: "Développeur Fullstack JS",
+    resumeUrl: "/Mothmora_2025.pdf",
+    avatarUrl: "/steve.jpg",
+  },
+  socials: [
+    { name: "GitHub", url: "https://github.com/Steviggio", iconName: "github" },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/stevemothmora/",
+      iconName: "linkedin",
+    },
+  ],
+  projects: [
+    {
+      title: "Polypost",
+      description: "Tableau de bord temps réel avec Next.js 14 et Tremor.",
+      link: "https://github.com/Steviggio/polypost",
+      active: true,
+      isSideProject: true,
+      tech: ["Next.js", "Tremor"],
+    },
+    {
+      title: "E-commerce Headless ",
+      description:
+        "Template E-commerce 'Headless' optimisé pour la performance.",
+      link: "https://github.com/Steviggio/eshop-template",
+      active: true,
+      isSideProject: true,
+      tech: ["Next.js", "Tremor"],
+    },
+    {
+      title: "Speakio",
+      description: "Plateforme d'accompagnement à l'apprentissage des langues.",
+      link: "https://github.com",
+      active: false,
+      isSideProject: true,
+    },
+    {
+      title: "Portfolio Minimaliste",
+      description: "Le site sur lequel vous naviguez actuellement.",
+      link: "https://github.com/Steviggio/next-portfolio",
+      active: true,
+      isSideProject: true,
+      tech: ["Next.js", "Tailwind CSS", "TypeScript", "Zod"],
+    },
+    {
+      title: "Ganatan",
+      description: "Plateforme de formation Fullstack Angular/Node.",
+      link: "https://ganatan.com",
+      active: true,
+      companyLogoUrl: "/ganatan-logo.png",
+      company: "Ganatan",
+      tech: ["Angular", "Node.js"],
+      isSideProject: false,
+    },
+    {
+      title: "Qobook",
+      description: "Librairie mobile pour découvrir et lire des livres.",
+      link: "https://qobook.app",
+      active: true,
+      companyLogoUrl: "/efficience-logo.png",
+      company: "Efficience Digitale",
+      tech: ["React Native", "Redux Toolkit", "Supabase"],
+      isSideProject: false,
+    },
+    {
+      title: "365 fois plus de laicité",
+      description: "Application native de sensibilisation à la laïcité.",
+      link: "https://365fois.com",
+      active: true,
+      companyLogoUrl: "/365fois-logo.png",
+      company: "365fois",
+      tech: ["React Native", "Redux Toolkit", "Strapi", "Mistral AI"],
+      isSideProject: false,
+    },
+  ],
+  experiences: [
+    {
+      company: "Efficience Digitale",
+      role: "Développeur Frontend",
+      date: "Sept. 2024 — Oct. 2025",
+      link: "https://efficience.com",
+    },
+    {
+      company: "Ganatan",
+      role: "Développeur Fullstack",
+      date: "Août 2022 - Sept. 2023",
+      link: "https://ganatan.com",
+    },
+    {
+      company: "Freelance",
+      role: "Développeur Web",
+      date: "Août 2023 - Présent",
+    },
+  ],
+};
+
+export const technologies = [
+  {
+    javascript: [
+      "React.js",
+      "React Native",
+      "Next.js",
+      "Node.js",
+      "TypeScript",
+    ],
+    python: ["Django", "Flask"],
+    databases: ["MongoDB", "PostgreSQL", "MySQL"],
+    devops: ["Docker", "Jenkins", "AWS", "Nginx"],
+  },
+];
+
+export class PortfolioRepository {
+  static async getPortfolio(): Promise<PortfolioData> {
+    const result = PortfolioDataSchema.safeParse(RAW_DATA);
+
+    if (!result.success) {
+      console.error("Invalid portfolio Data: ");
+      throw new Error("Data integrity check failed");
+    }
+    return result.data;
+  }
+}
---
+++ src/features/portfolio/types/portfolio.schema.ts
@@ -0,0 +1,55 @@
+// src/features/portfolio/types/portfolio.schema.ts
+import { z } from "zod";
+
+// --- Primitives ---
+export const SocialLinkSchema = z.object({
+  name: z.string(),
+  url: z.url(),
+  // On stockera le nom de l'icône (string) plutôt que le composant React pour sérialiser facilement les données
+  iconName: z.enum(["github", "linkedin", "twitter", "default"]),
+});
+
+export const ProjectSchema = z.object({
+  title: z.string().min(1),
+  description: z.string(),
+  link: z.url(),
+  active: z.boolean().default(true),
+  company: z.string().optional(),
+  companyLogoUrl: z.string().optional(),
+  tech: z.array(z.string()).default([]),
+  isSideProject: z.boolean().default(false), // Pour différencier Projets Perso vs Entreprise
+});
+
+export const ExperienceSchema = z.object({
+  company: z.string(),
+  role: z.string(),
+  date: z.string(), // Pourrait être z.date() mais string est plus simple pour l'affichage tel quel
+  link: z.url().optional(),
+  description: z.string().optional(),
+});
+
+export const ProfileSchema = z.object({
+  name: z.string(),
+  nickname: z.string(),
+  title: z.string(),
+  about: z.string(),
+  description: z.string(),
+  location: z.string(),
+  role: z.string(),
+  resumeUrl: z.string(),
+  avatarUrl: z.string(),
+});
+
+// --- Aggregates ---
+export const PortfolioDataSchema = z.object({
+  profile: ProfileSchema,
+  socials: z.array(SocialLinkSchema),
+  projects: z.array(ProjectSchema),
+  experiences: z.array(ExperienceSchema),
+});
+
+// Inference TS automatique
+export type Project = z.infer<typeof ProjectSchema>;
+export type Experience = z.infer<typeof ExperienceSchema>;
+export type PortfolioData = z.infer<typeof PortfolioDataSchema>;
+export type Profile = z.infer<typeof ProfileSchema>;
