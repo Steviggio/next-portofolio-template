71ac1e1b42d40aeca4733cad85a2136737b6e04e
Steviggio <steve.mothmora@gmail.com>
2026-02-27 16:20:17 +0100

    fix(type): readonly prop added for portfolio's datas for immutability

--- src/features/portfolio/types/portfolio.schema.ts
+++ src/features/portfolio/types/portfolio.schema.ts
@@ -1,11 +1,8 @@
-// src/features/portfolio/types/portfolio.schema.ts
 import { z } from "zod";
 
-// --- Primitives ---
 export const SocialLinkSchema = z.object({
   name: z.string(),
   url: z.url(),
-  // On stockera le nom de l'icône (string) plutôt que le composant React pour sérialiser facilement les données
   iconName: z.enum(["github", "linkedin", "twitter", "default"]),
 });
 
@@ -18,14 +15,14 @@ export const ProjectSchema = z.object({
   company: z.string().optional(),
   companyLogoUrl: z.string().optional(),
   tech: z.array(z.string()).default([]),
-  isSideProject: z.boolean().default(false), // Pour différencier Projets Perso vs Entreprise
+  isSideProject: z.boolean().default(false), 
 });
 
 export const ExperienceSchema = z.object({
   id: z.string(),
   company: z.string(),
   role: z.string(),
-  date: z.string(), // Pourrait être z.date() mais string est plus simple pour l'affichage tel quel
+  date: z.string(),
   link: z.url().optional(),
   description: z.string().optional(),
 });
@@ -42,15 +39,15 @@ export const ProfileSchema = z.object({
   avatarUrl: z.string(),
 });
 
-// --- Aggregates ---
-export const PortfolioDataSchema = z.object({
-  profile: ProfileSchema,
-  socials: z.array(SocialLinkSchema),
-  projects: z.array(ProjectSchema),
-  experiences: z.array(ExperienceSchema),
-});
+export const PortfolioDataSchema = z
+  .object({
+    profile: ProfileSchema,
+    socials: z.array(SocialLinkSchema),
+    projects: z.array(ProjectSchema),
+    experiences: z.array(ExperienceSchema),
+  })
+  .readonly();
 
-// Inference TS automatique
 export type Project = z.infer<typeof ProjectSchema>;
 export type Experience = z.infer<typeof ExperienceSchema>;
 export type PortfolioData = z.infer<typeof PortfolioDataSchema>;
